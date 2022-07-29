2e5cada66131295979eecc6be26dd741891f0ee1
mynameisdaun <daun9870.jung@gmail.com>
2022-07-29 15:48:43 +0900

    <feat>(assets/images) add real english-word images book

--- src/dummyData.js
+++ src/dummyData.js
@@ -12,7 +12,7 @@ const capacity = new Word(5, 'capacity', '용량');
 const conscious = new Word(6, 'conscious', '의식하는, 자각하는');
 const damp = new Word(7, 'damp', '축축한');
 const brief = new Word(8, 'brief', '간략한');
-const calibre = new Word(9, 'calibre','역량');
+const calibre = new Word(9, 'calibre', '역량');
 const words = [justify, accurate, capable, personnel, capacity, conscious, damp, brief, calibre];
 
 const createDummyWrongAnswers = (word) => {
@@ -21,7 +21,7 @@ const createDummyWrongAnswers = (word) => {
 		let j = Math.floor(Math.random() * (i + 1));
 		[wrongAnswers[i], wrongAnswers[j]] = [wrongAnswers[j], wrongAnswers[i]];
 	}
-	return wrongAnswers.slice(0,3);
+	return wrongAnswers.slice(0, 3);
 }
 
 const DUMMY_QUIZS = new Quizs([
@@ -37,7 +37,13 @@ const DUMMY_QUIZS = new Quizs([
 ]);
 
 const DUMMY_QUIZS_WITH_USER_ANSWER = new Quizs(Array.from(DUMMY_QUIZS.quizs));
-DUMMY_QUIZS_WITH_USER_ANSWER.quizs.forEach(quiz=>quiz.saveUserChoice([quiz.word,...quiz.wrongChoices][Math.floor(Math.random() * 4)]));
-const DUMMY_WORKBOOKS = new Workbook('공무원 9급 준비 필수 영단어', DUMMY_QUIZS);
+DUMMY_QUIZS_WITH_USER_ANSWER.quizs.forEach(quiz => quiz.saveUserChoice([quiz.word, ...quiz.wrongChoices][Math.floor(Math.random() * 4)]));
+
+const DUMMY_WORKBOOKS = [
+	new Workbook(1, '해커스 토익 빈출', DUMMY_QUIZS, '정찬울', Date.now(), Date.now(), '/assets/images/hackers_toeic.jpeg'),
+	new Workbook(2, '경선식 영단어', DUMMY_QUIZS, '정다운', Date.now(), Date.now(), '/assets/images/kyeong.jpeg'),
+	new Workbook(3, '어휘 끝', DUMMY_QUIZS, '최해미', Date.now(), Date.now(), '/assets/images/word_done.jpeg'),
+	new Workbook(4, '워드 마스터', DUMMY_QUIZS, '송주하', Date.now(), Date.now(), '/assets/images/word_master.jpeg')
+];
 
 export {DUMMY_WORKBOOKS, DUMMY_QUIZS, DUMMY_QUIZS_WITH_USER_ANSWER};
